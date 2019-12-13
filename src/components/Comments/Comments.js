530be159873ec4c8bebd876b9e528f6c8821106a
Roey Shmool <roey.shmool@gmail.com>
2019-12-13 15:44:19 -0600

    add component folders and files

---
+++ src/components/Comments/Comments.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Comments extends Component {
+  render() {
+    return (
+
+    )
+  }
+}
+
+export default connect()(Comments);
