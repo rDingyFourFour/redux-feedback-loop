ce56e2562df72f4df8075cb1a99f52ccfc7c2f36
Roey Shmool <roey.shmool@gmail.com>
2019-12-14 14:25:05 -0600

    add reducer, dispatch

--- src/components/Feeling/Feeling.js
+++ src/components/Feeling/Feeling.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
 import {withRouter} from 'react-router-dom';
+import {connect} from 'react-redux';
 
 class Feeling extends Component {
 
-  handleClick = () => 
+  state = {
+    newFeeling: '',
+  }
+  
+  handleChange = (event) => {
+    console.log('handleChange')
+    this.setState({ newFeeling: event.target.value });  
+  } 
+    
+  handleClick = () => {
+    console.log('handleClick');
     this.props.history.push('/understanding');
-
+    this.props.dispatch({ type: 'FEELINGS', payload: this.state.newFeeling });
+  }
+    
   render() {
     return (
       <div>
         <h3>How are you feeling today?</h3>
         <label>Feeling?</label>
-        <input type="number"></input>
+        <input type="number" onChange={this.handleChange}></input>
         <button onClick={this.handleClick}>Next</button>
       </div>
     )
   }
 }
 
-export default withRouter(Feeling);
+export default (withRouter, connect())(Feeling);
--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
+import logger from 'redux-logger';
 
 import './index.css';
 import App from './components/App/App';
 
-
-
+const feelings = (state={}, action) => {
+  if(action.type === 'FEELINGS') {
+    console.log('this is feelings', action.payload);
+    return action.payload;
+  } 
+  return state;
+}
 
 const storeInstance = createStore(
-  () => {
-    console.log('store instance'); 
-  },
+  combineReducers({
+    feelings, 
+  }),  
+  applyMiddleware(logger)
 );
 
 ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, 
